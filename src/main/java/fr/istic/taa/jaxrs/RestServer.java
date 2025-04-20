291815be1edd258e6c9bacb981381b058359e55f
Alvine2545 <your-email@example.com>
2025-04-21 00:15:20 +0200

    resolved conflict

--- src/main/java/fr/istic/taa/jaxrs/RestServer.java
+++ src/main/java/fr/istic/taa/jaxrs/RestServer.java
@@ -12,7 +12,6 @@ import java.util.logging.Logger;
 public class RestServer {
 
     private static final Logger logger = Logger.getLogger(RestServer.class.getName());
-
     public static void main( String[] args ) {
 
         UndertowJaxrsServer ut = new UndertowJaxrsServer();
